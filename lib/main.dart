92af80f6228b20381a22e699a24a9b61fcf6f82c
wuhui <wuhui@yadingdata.com>
2020-04-26 17:55:04 +0800

    状态组件-点击修改变量

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,16 +14,56 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  
- @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("wo shi title"),
-        ),
-        body: Center(child: Text('Hello world')),
-    );
-  }
+
+class MyHomePage extends StatefulWidget{
+   @override
+  MyHomePageState createState() => MyHomePageState();
 }
+
+class MyHomePageState extends State<MyHomePage>{
+     var msg="Hello World"; //msg默认文字
+     int _counter = 0;
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                        title: Text("我是Title"),
+                      ),
+            body:Center(child: Column(
+              children: <Widget>[
+                Text(msg),
+                FlatButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: (){
+                    setState(() {
+                      _counter++;
+                      this.msg = 'click times '+'$_counter';
+                    });
+                  },
+                  child: Text(
+                    "click me",
+                    style: TextStyle(fontSize: 14.0),
+                  
+                  ),
+
+                )
+              ],),
+              )
+        );
+      }
+
+}
+// class MyHomePage extends StatelessWidget {
+  
+//  @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text("wo shi title"),
+//         ),
+//         body: Center(child: Text('Hello world')),
+//     );
+//   }
+// }
 
